f0790135d4324780a990f7da13b09704f10527af
sugiura-hiromiti <pishadon57@gmail.com>
2026-09-21 06:18:55 +0900

    complete minimal implementation of nix/ci.nix

--- nix/ci.nix
+++ nix/ci.nix
@@ -4,9 +4,23 @@
       enable = true;
       workflows = {
         ci = {
+          name = "ci";
+          on = {
+            push = {
+              branches = [ "main" ];
+            };
+            pullRequest = {
+              branches = [ "main" ];
+            };
+          };
           jobs = {
             check = {
-              steps = [ { run = "nix flake check -L --show-trace"; } ];
+              runsOn = "ubuntu-latest";
+              steps = [
+                { uses = "actions/checkout@v4"; }
+                { uses = "cachix/install-nix-action@v24"; }
+                { run = "nix flake check -L --show-trace"; }
+              ];
             };
           };
         };
